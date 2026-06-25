3978beb9276ba8e44f710f3427f18b6713f6a401
TuUsuario <tu-correo@gmail.com>
2026-06-25 06:02:34 -0500

    completar archivo manifets y models

---
+++ Proyecto_Nestle/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Proyecto_Nestle',
+    'version': '1.0',
+    'summary': 'Proyecto Nestle',
+    'description': 'Mi Proyecto final de Nestle',
+    'category': 'sales',
+    'author': 'Nicole.R',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/productos_views.xml'
+    ]
+    
+}
